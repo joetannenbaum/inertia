3652e7b8a6c88301408abb01695dda399814d810
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-07-08 14:47:48 -0400

    using new deferred component

--- playgrounds/vue3/resources/js/Pages/Defer.vue
+++ playgrounds/vue3/resources/js/Pages/Defer.vue
@@ -1,4 +1,5 @@
 <script lang="ts">
+import Deferred from '../../../../../packages/vue3/src/Deferred.vue'
 import Layout from '../Components/Layout.vue'
 export default { layout: Layout }
 </script>
@@ -33,30 +34,39 @@ defineProps<{
 
   <div class="flex mt-6 space-x-6">
     <div class="w-1/2 p-4 border border-black rounded">
-      <div v-if="!users">
-        <p>Loading Users...</p>
-      </div>
-      <div v-else v-for="user in users">
-        <p>#{{ user.id }}: {{ user.name }} ({{ user.email }})</p>
-      </div>
+      <Deferred :data="users">
+        <template #loading>
+          <p>Loading Users...</p>
+        </template>
+
+        <div v-for="user in users">
+          <p>#{{ user.id }}: {{ user.name }} ({{ user.email }})</p>
+        </div>
+      </Deferred>
     </div>
 
     <div class="w-1/2 p-4 border border-black rounded">
-      <div v-if="!foods">
-        <p>Loading Foods...</p>
-      </div>
-      <div v-else v-for="food in foods">
-        <p>#{{ food.id }}: {{ food.name }}</p>
-      </div>
+      <Deferred :data="foods">
+        <template #loading>
+            <p>Loading Foods...</p>
+        </template>
+
+        <div v-for="food in foods">
+            <p>#{{ food.id }}: {{ food.name }}</p>
+        </div>
+      </Deferred>
     </div>
 
     <div class="w-1/2 p-4 border border-black rounded">
-      <div v-if="!organizations">
-        <p>Loading Organizations...</p>
-      </div>
-      <div v-else v-for="org in organizations">
-        <p>#{{ org.id }}: {{ org.name }} ({{ org.url }})</p>
-      </div>
+      <Deferred :data="organizations">
+        <template #loading>
+            <p>Loading Organizations...</p>
+        </template>
+
+        <div  v-for="org in organizations">
+            <p>#{{ org.id }}: {{ org.name }} ({{ org.url }})</p>
+        </div>
+      </Deferred>
     </div>
   </div>
 </template>
